c2e9bc0e7bf10fd2cdce339c9ea475d14656dbd9
Franco Redoni <lennarx@outlook.com>
2022-05-30 21:35:55 -0300

    cambios en estilos

--- vue-marketplace/src/components/unauthorized/Unauthorized.vue
+++ vue-marketplace/src/components/unauthorized/Unauthorized.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="align-items: center;margin: 25rem 60rem 30rem 60rem;">
     <p>Usuario o contraseña incorrecta</p>
     <button class="btn btn-info mt-4 text-white" v-on:click="this.redirectToLogin()">LOGUEATE</button>
   </div>
